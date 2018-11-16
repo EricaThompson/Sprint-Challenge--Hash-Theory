330edaa9da3673fddb660d695bc912518e9aa13f
Erica <Erica@BlufordsonSrMBP.lan>
2018-11-16 17:21:12 -0500

    After Lecture

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -7,12 +7,29 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
 
-  // YOUR CODE HERE
+  for (int i = 0; i < length ; i++){
+    int complement = hash_table_retrieve(ht, limit - weights[i]);
+    if (complement != -1){
+      //found match
+      Answer *answer = malloc(sizeof(Answer));
+      answer->index_1 = i;
+      answer->index_2 = complement;
+      destroy_hash_table(ht);
+      return answer;
+    } else {
+      //insert into the hash tables
+      //the weight is the key, and the array index is the value
+      hash_table_insert(ht, weights[i], i);
+    }
 
+  }
 
-
+  destroy_hash_table(ht);
   return NULL;
 }
+
+//limit is 21
+// {4, 6, 10, 15, 16}
 
 void print_answer(Answer *answer)
 {
